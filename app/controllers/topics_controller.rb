d3057713246acf6a19b12ac3a6bdd168e698b91d
darenang44 <darenang44@gmail.com>
2016-12-15 15:32:17 -0800

    Add new and create action with views

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,31 @@
 class TopicsController < ApplicationController
+  # before_action: :find_topic, only: [:show]
 
   def index
   end
-  
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def find_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:title, :description)
+  end
+
 end
